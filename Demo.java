00c6d405c26ed284697632dc1df3df9211dfb8a6
Stacy <skim10@stuy.edu>
2020-10-22 00:00:47 -0400

    Adding arrToString and arrayDeepToString

--- Demo.java
+++ Demo.java
@@ -7,7 +7,30 @@ public class Demo{
       System.out.println();
     }
   }
-  public static void main(String[]args){
-    printLoop(5);
+  public static String arrToString(int[] arr){
+    String answer = "{";
+    for (int i = 0; i < arr.length; i++){
+      answer = answer + arr[i];
+      if (i < arr.length-1){
+        answer = answer + ", ";
+      }
+    }
+    return answer + "}";
+  }
+  public static String arrayDeepToString(int[][] arr){
+    String answer = "{";
+    for (int i = 0; i < arr.length; i++){
+      answer += "{";
+      for (int j = 0; j < arr[i].length; j++){
+        answer += arr[i][j];
+        if (j < arr[i].length-1){
+          answer = answer + ", ";
+        }
+      }
+      if (i < arr.length-1){
+        answer = answer + "}, ";
+      }
+    }
+    return answer + "}}";
   }
 }
